f2cf82dfae2c540501e6d3c3c6265b5add496422
Justin Hall <hall.justin.d@gmail.com>
2019-04-17 02:08:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,3 +3,13 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES (null, null, null);
 INSERT INTO series (title, author_id, subgenre_id) VALUES (null, null, null);
 
 INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
