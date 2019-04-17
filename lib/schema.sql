80be3268e50d82b594558a261ae7d91dd3105cb0
Justin Hall <hall.justin.d@gmail.com>
2019-04-17 02:03:34 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,3 +14,24 @@ CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year TEXT,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
+);
