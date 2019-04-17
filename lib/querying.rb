02ddf9429aa7ce2421b2e3caec6d5492558a46a6
Justin Hall <hall.justin.d@gmail.com>
2019-04-17 02:36:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) FROM characters JOIN series ON characters.series_id = series.id JOIN books ON books.series_id = series.id"
+  "SELECT characters.name, COUNT(*) AS total FROM characters JOIN series ON characters.series_id = series.id JOIN books ON books.series_id = series.id"
 end
