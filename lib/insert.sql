2a76e0d2be5f424742c7be73b366960c4e8af251
Justin Hall <hall.justin.d@gmail.com>
2019-04-17 02:09:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -13,3 +13,19 @@ INSERT INTO books (title, year, series_id) VALUES (null, null, null);
 INSERT INTO books (title, year, series_id) VALUES (null, null, null);
 
 INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
